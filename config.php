b0fa58d9e615c6428e16fa06272dc810d0324b1e
Matyáš Koc <admin@sufix.cz>
2017-08-06 13:36:05 +0200

    Update config

--- config.php
+++ config.php
@@ -1,7 +1,15 @@
 <?php
 
 define("_SRV", "localhost");
-define("_USR", "websys");
-define("_PW", "D4VWaVlF76TTKeaK");
-define("_DB", "websys");
+define("_USR", "DBuser");
+define("_PW", "DBpass");
+define("_DB", "DBname");
 
+define("_SMTPSRV", "SMTPserver");
+define("_SMTPUSR", "SMTPuser");
+define("_SMTPPASS", "SMTPpass");
+define("_SMTPPROTO", "tls");
+define("_SMTPPORT", 587);
+define("_SMTPFROMADDR", "SMTPfromaddress");
+define("_SMTPFROMNAME", "SMTPfromname");
+define("_SMTPCERTINVALID", true);
